0e62bbc7f293a99c44cf1ad03a002cdecaa028ab
Ruslan Uteshev <dolphin_4@mail.ru>
2021-01-20 20:34:06 +0300

    tests with parametrization

--- pages/locators.py
+++ pages/locators.py
@@ -7,5 +7,15 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    MESSAGE_ADD_TO_BASKET = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-success:nth-child(1)")
+    THE_BOOK_TITLE = (By.CSS_SELECTOR, "div.col-sm-6.product_main>h1")
+    THE_BOOK_COST = (By.CSS_SELECTOR, "div.col-sm-6.product_main>p")
+    THE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-info .alertinner>p")
+    
+    
+    
     
     
---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    
+    def add_to_basket(self):
+        basket_link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        basket_link.click()
+
+    
+    def book_title_should_match_the_message(self):
+        book_title = self.browser.find_element(*ProductPageLocators.THE_BOOK_TITLE)
+        message_add_to_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_ADD_TO_BASKET)
+        assert book_title.text + " " + "book"  != ' '.join(message_add_to_basket.text.split() [1:5]), "The book title is absent in adding to basket message"
+        
+    def book_cost_should_match_the_basket_total(self):    
+        book_cost = self.browser.find_element(*ProductPageLocators.THE_BOOK_COST)
+        basket_total = self.browser.find_element(*ProductPageLocators.THE_BASKET_TOTAL)
+        assert book_cost.text.split(" £") [0] == basket_total.text.split(" ") [4], "The book cost is not equal to basket total cost"
+        
